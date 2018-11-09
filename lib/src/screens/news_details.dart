1eedd29cf65306b804d041458574055c2b30770b
Manvendra Singh <manvendrask2@gmail.com>
2018-11-10 00:03:37 +0530

    Showing the title.

--- lib/src/screens/news_details.dart
+++ lib/src/screens/news_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/comments_provider.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -6,11 +8,35 @@ class NewsDetail extends StatelessWidget {
   NewsDetail({this.itemId});
 
   Widget build(BuildContext context) {
+    CommentsBloc bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Details'),
       ),
-      body: Text('some text for details $itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (BuildContext context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading');
+        }
+
+        return FutureBuilder(
+          future: snapshot.data[itemId],
+          builder: (BuildContext context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('Loading');
+            }
+
+            return Text(itemSnapshot.data.title);
+          },
+        );
+      },
     );
   }
 }
