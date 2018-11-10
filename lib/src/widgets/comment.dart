19b2ba8fc4231acae89ff9b7085b55d558fdeff5
Manvendra Singh <manvendrask2@gmail.com>
2018-11-10 17:55:04 +0530

    Handling for the deleted comment.

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -20,7 +20,7 @@ class Comment extends StatelessWidget {
         final List<Widget> children = [
           ListTile(
             title: Text(item.text),
-            subtitle: Text(item.by),
+            subtitle: item.by == '' ? Text('Deleted') : Text(item.by),
           ),
           Divider()
         ];
